e24fe2d309b8b38c285155b15fc2f2feabd47e15
Zachary Spar <41600414+zachspar@users.noreply.github.com>
2024-07-08 19:35:17 -0400

    Fix/iter (#4)
    * update readme
    
    * add another sentence
    
    * fix iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-use pyo3::prelude::*;
-use pyo3::types::{PyDict, PyList, PyBool};
 use parquet::file::reader::{FileReader, SerializedFileReader};
-use std::{fs::File, path::Path};
-use parquet::record::Row;
+use parquet::record::reader::RowIter;
 use pyo3::exceptions::PyStopIteration;
+use pyo3::prelude::*;
+use pyo3::types::{PyBool, PyDict, PyList};
 use serde_json::Value;
+use std::{fs::File, path::Path};
 
 struct PyValue(Value);
 
@@ -32,7 +32,7 @@ fn value_to_py_object(py: Python, value: &Value) -> PyResult<PyObject> {
                     "Unsupported number type",
                 ))
             }
-        },
+        }
         Value::String(s) => Ok(s.into_py(py)),
         Value::Array(arr) => {
             let py_list = PyList::empty_bound(py);
@@ -40,14 +40,14 @@ fn value_to_py_object(py: Python, value: &Value) -> PyResult<PyObject> {
                 py_list.append(value_to_py_object(py, item)?)?;
             }
             Ok(py_list.into_py(py))
-        },
+        }
         Value::Object(obj) => {
             let py_dict = PyDict::new_bound(py); // Correct usage of PyDict
             for (k, v) in obj {
                 py_dict.set_item(k, value_to_py_object(py, v)?)?;
             }
             Ok(py_dict.into_py(py))
-        },
+        }
     }
 }
 
@@ -55,31 +55,28 @@ fn value_to_py_object(py: Python, value: &Value) -> PyResult<PyObject> {
 #[pyfunction]
 fn to_json_str(path: &str) -> PyResult<String> {
     let file_path = Path::new(path);
-    if let Ok(file) = File::open(&file_path) {
-        let reader = SerializedFileReader::new(file).unwrap();
+    let file =
+        File::open(&file_path).map_err(|e| pyo3::exceptions::PyIOError::new_err(e.to_string()))?;
+    let reader = SerializedFileReader::new(file)
+        .map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?;
 
-        // iterate through reader and add to json list string
-        let mut json_str = "[".to_string();
-        for row in reader.get_row_iter(None).unwrap() {
-            json_str.push_str(&row.unwrap().to_json_value().to_string());
-            json_str.push_str(",");
-        }
-
-        json_str.pop();
-        json_str.push_str("]");
-
-        // return json string
-        return Ok(json_str);
-    } else {
-        // return ValueError if file not found
-        Err(PyErr::new::<pyo3::exceptions::PyValueError, _>("Unable to open parquet file"))
+    // iterate through reader and add to json list string
+    let mut json_str = "[".to_string();
+    for row in reader.get_row_iter(None).unwrap() {
+        json_str.push_str(&row.unwrap().to_json_value().to_string());
+        json_str.push_str(",");
     }
+
+    json_str.pop();
+    json_str.push_str("]");
+
+    // return json string
+    return Ok(json_str);
 }
 
 #[pyclass]
 struct ParquetRowIterator {
-    // FIXME: This should be a RowIter instead
-    iter: std::vec::IntoIter<Row>
+    iter: RowIter<'static>,
 }
 
 #[pymethods]
@@ -87,11 +84,14 @@ impl ParquetRowIterator {
     #[new]
     fn new(path: &str) -> PyResult<Self> {
         let file_path = Path::new(path);
-        let file = File::open(&file_path).map_err(|e| pyo3::exceptions::PyIOError::new_err(e.to_string()))?;
-        let reader = SerializedFileReader::new(file).map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?;
+        let file = File::open(&file_path)
+            .map_err(|e| pyo3::exceptions::PyIOError::new_err(e.to_string()))?;
+        let reader = SerializedFileReader::new(file)
+            .map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?;
 
-        // FIXME: This should be a RowIter instead of rendering vec and then providing an iterator!!!
-        Ok(Self { iter: reader.get_row_iter(None).unwrap().map(|r| r.unwrap()).collect::<Vec<_>>().into_iter()})
+        Ok(Self {
+            iter: RowIter::from_file_into(Box::new(reader)),
+        })
     }
 
     fn __iter__(slf: PyRef<Self>) -> PyRef<Self> {
@@ -99,8 +99,11 @@ impl ParquetRowIterator {
     }
 
     fn __next__(mut slf: PyRefMut<Self>) -> PyResult<PyObject> {
-        let row = slf.iter.next().ok_or_else(|| PyErr::new::<PyStopIteration, _>("End of iterator"))?;
-        let row_dict = row.to_json_value();
+        let row = slf
+            .iter
+            .next()
+            .ok_or_else(|| PyErr::new::<PyStopIteration, _>("No more rows in parquet file"))?;
+        let row_dict = row.unwrap().to_json_value();
         let dict = PyDict::new_bound(slf.py());
         for (key, value) in row_dict.as_object().unwrap() {
             dict.set_item(key, PyValue(value.clone()))?;
@@ -112,32 +115,34 @@ impl ParquetRowIterator {
 #[pyfunction]
 fn to_iter(path: &str) -> PyResult<ParquetRowIterator> {
     let file_path = Path::new(path);
-    let file = File::open(&file_path).map_err(|e| pyo3::exceptions::PyIOError::new_err(e.to_string()))?;
-    let reader = SerializedFileReader::new(file).map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?;
+    let file =
+        File::open(&file_path).map_err(|e| pyo3::exceptions::PyIOError::new_err(e.to_string()))?;
+    let reader = SerializedFileReader::new(file)
+        .map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?;
 
-    Ok(ParquetRowIterator { iter: reader.get_row_iter(None).unwrap().map(|r| r.unwrap()).collect::<Vec<_>>().into_iter()})
+    Ok(ParquetRowIterator {
+        iter: RowIter::from_file_into(Box::new(reader)),
+    })
 }
 
 #[pyfunction]
 fn to_list(path: &str, py: Python) -> PyResult<PyObject> {
     let file_path = Path::new(path);
-    if let Ok(file) = File::open(&file_path) {
-        let reader = SerializedFileReader::new(file).unwrap();
-
-        let list = PyList::empty_bound(py);
-        for row in reader.get_row_iter(None).unwrap() {
-            let row_dict = row.unwrap().to_json_value();
-            let dict = PyDict::new_bound(py);
-            for (key, value) in row_dict.as_object().unwrap() {
-                dict.set_item(key, PyValue(value.clone()))?;
-                list.append(&dict)?;
-            }
+    let file =
+        File::open(&file_path).map_err(|e| pyo3::exceptions::PyIOError::new_err(e.to_string()))?;
+    let reader = SerializedFileReader::new(file)
+        .map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?;
+    let list = PyList::empty_bound(py);
+    for row in reader.get_row_iter(None).unwrap() {
+        let row_dict = row.unwrap().to_json_value();
+        let dict = PyDict::new_bound(py);
+        for (key, value) in row_dict.as_object().unwrap() {
+            dict.set_item(key, PyValue(value.clone()))?;
+            list.append(&dict)?;
         }
-
-        Ok(list.into())
-    } else {
-        Err(PyErr::new::<pyo3::exceptions::PyValueError, _>("Unable to open parquet file"))
     }
+
+    Ok(list.into())
 }
 
 // python module
